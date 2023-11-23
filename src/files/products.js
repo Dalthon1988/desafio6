a0b9b077520ecfc7f6354aebf63ece92a388c96d
Diego Chavez <dalthon1988@gmail.com>
2023-11-23 20:24:29 -0300

    desafio6

---
+++ src/files/products.js
@@ -0,0 +1,117 @@
+const products = [
+    {
+        "title": "Cuadro1",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "001",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "6d1g_HjKfKerrqh4qpYOE"
+      },
+      {
+        "title": "Cuadro2",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "001",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "pP_m_CmNrYAf33EZBksQ7"
+      },
+      {
+        "title": "Cuadro3",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "002",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "m32s6mcfCK2MRMCNJZ5lB"
+      },
+      {
+        "title": "Cuadro4",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "003",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "Psgb5RO73Pq93cWJBo-8F"
+      },
+      {
+        "title": "Cuadro5",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "004",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "jUfJA_AMeULDd1cYI6YuI"
+      },
+      {
+        "title": "Cuadro6",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "005",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "iUxFYro1lnOwlcnkmkNIh"
+      },
+      {
+        "title": "Cuadro7",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "006",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "zLHY0dUANjCKGlyUAmtS_"
+      },
+      {
+        "title": "Cuadro8",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "007",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "4R1zV7HEga2cDfmDHVrs5"
+      },
+      {
+        "title": "Cuadro9",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "008",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "3coRA0InfKAiBbzFPgxHD"
+      },
+      {
+        "title": "Cuadro10",
+        "description": "Aca seria la descripcion cuando quede el producto agregado",
+        "code": "009",
+        "price": "0000",
+        "status": "True",
+        "stock": "0000",
+        "category": "Obra de arte",
+        "thumbnails": "Array",
+        "id": "mDiRnH5I4GSTixohzihdh"
+      }
+
+
+
+]
+
+export default products;
